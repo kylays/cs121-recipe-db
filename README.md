bb9b222608cefa4342db26a52178cc118b77c6db
kylays <kyla.yu.swanson@gmail.com>
2023-03-16 02:29:56 -0700

    data, er, ddl work in progress

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
 # recipe-db
+TODO - instructions on how to use the app
+
+
+
+notes:
+- I removed the ingredient table since it didn't make sense why we had an ingredient name and id when these were just one to one
+- will be replacing that table with chefs since need 5 tables
+
+- not sure if non chef users have features for er diagram?? do they have a table for preferences? tracking their usage???
+- also the er diagram has some relationships that i am unsure of (might just delete them if not going to implement them anyways bc extra)
+
+
+kyla
+* ER diagram 
+  - DONE, sort of? I'm kind of unsure about it 
+* DDL 
+  - DONE
+* load-data + csv files 
+  - (missing csv for the extra tables ratings, cost, chefs and users)
+  - TODO users, chefs
+* passwords 
+  - TODO THURSDAY
+* grant permissions 
+  - TODO THURSDAY
+* queries 
+  - TODO THURSDAY after finish load-data
+
+riya
+* procedural sql
+* app.py
+
+together
+* reflection.pdf (do this weekend)
+  - functional dependencies
+  - relational algebra
+  - other
+* README.md instructions
---
+++ setup.sql
@@ -0,0 +1,80 @@
+-- CS 121 Final Project
+-- Kyla Yu-Swanson and Riya Shrivastava 
+
+DROP TABLE IF EXISTS user;
+DROP TABLE IF EXISTS recipe;
+DROP TABLE IF EXISTS categories;
+DROP TABLE IF EXISTS ingredient;
+DROP TABLE IF EXISTS compound_ingredient;
+DROP TABLE IF EXISTS ingredient_amounts;
+DROP TABLE IF EXISTS cost;
+
+CREATE TABLE users (
+  user_id             VARCHAR(100),
+  first_name          VARCHAR(100),
+  last_name           VARCHAR(100),
+  password_phrase     VARCHAR(100) NOT NULL
+  PRIMARY KEY (user_id)
+);
+
+CREATE TABLE chefs (
+  user_id             VARCHAR(100),
+  -- a rating such as amateur, professional, homecook, ect.
+  experience_level    VARCHAR(100),
+  -- a chef's area of expertise, such as pasty chefs being experts in pastries
+  specialization      VARCHAR(100),
+  PRIMARY KEY (user_id),
+  FOREIGN KEY (user_id) REFERENCES users
+)
+
+CREATE TABLE recipes (
+  recipe_id         INT AUTO_INCREMENT,
+  recipe_name       VARCHAR(100) NOT NULL,
+  subcategory       VARCHAR(100) NOT NULL,
+  directions        VARCHAR(5000) NOT NULL,
+  PRIMARY KEY (recipe_id)
+);
+
+CREATE TABLE categories (
+  category           VARCHAR(100) NOT NULL,
+  subcategory        VARCHAR(100) NOT NULL,
+  PRIMARY KEY (category, subcategory),
+  FOREIGN KEY (subcategory) REFERENCES recipes
+);
+
+CREATE TABLE ingredient_amounts (
+  recipe_id         INT,
+  ingredient_name   VARCHAR(100) NOT NULL,
+  quantity          NUMERIC(5, 3) NOT NULL,
+  -- unit may be null for items such as egg where the egg is a unit
+  unit              VARCHAR(50),
+  PRIMARY KEY (recipe_id, ingredient_name),
+  FOREIGN KEY (recipe_id) REFERENCES recipes
+);
+
+-- This is an extra feature that may or may not be implemented.
+CREATE TABLE ratings (
+  recipe_id         INT,
+  -- a rating out of 5
+  stars             NUMERIC(5, 0) NOT NULL,
+  rated_on          TIMESTAMP,
+  user_id           VARCHAR(100) NOT NULL,
+  review            VARCHAR(1000),
+  PRIMARY KEY (recipe_id),
+  FOREIGN KEY (recipe_id) REFERENCES recipes,
+  FOREIGN KEY (user_id) REFERENCES users
+);
+
+-- This is an extra feature that may or may not be implemented.
+CREATE TABLE cost (
+  ingredient_name   INT,
+  quantity          NUMERIC(5, 3) NOT NULL,
+  -- in dollars
+  price             NUMERIC(5, 2) NOT NULL,
+  unit              VARCHAR(50),
+  PRIMARY KEY (ingredient_name, quantity),
+  FOREIGN KEY (ingredient_name) REFERENCES ingredient_amounts
+);
+
+-- Indices 
+CREATE INDEX recipe_id_idx ON recipes (recipe_id);
